b6a8c829b144399be376f498bf42fc413a382c08
Daryna Serhieieva <dserhiei@c3r1s3.42urduliz.com>
2024-10-10 21:10:03 +0200

    added a few changes

--- client.c
+++ client.c
@@ -1,57 +1,46 @@
 #include "libft.h"
 #include "minitalk.h"
 
-void send_char(pid_t server_pid, char c)
+void send_message(pid_t server_pid, char c)
 {
-    for (int i = 0; i < 8; i++)
-    {
-        if (c & (1 << i))
-            kill(server_pid, SIGUSR2); // Send bit 1 as SIGUSR2
+	int	i;
+
+	i = 0;
+	while (i < 8)
+	{
+		if (c & (1 << i))
+            kill(server_pid, SIGUSR2);
         else
-            kill(server_pid, SIGUSR1); // Send bit 0 as SIGUSR1
-
-        usleep(100); // Give the server time to process the signal
-    }
-}
-
-int main(int argc, char **argv)
-{
-    if (argc != 3)
-    {
-        printf("Usage: %s <server_pid> <message>\n", argv[0]);
-        return 1;
-    }
-
-    pid_t server_pid = atoi(argv[1]);
-    char *message = argv[2];
-
-    // Send each character of the message
-    for (int i = 0; message[i] != '\0'; i++)
-    {
-        send_char(server_pid, message[i]);
-    }
-
-    return 0;
+            kill(server_pid, SIGUSR1);
+        usleep(100);
+		i++;
+	}
 }
 
 int	main(int argc, char **argv)
 {
-	char				message;
-	int					pid;
+	char				*message;
+	int					server_pid;
+    int					i;
 
-	pid = 0;
+	server_pid = 0;
+	i = 0;
 	if (argc != 3)
 	{
 		ft_printf("Too much or too few arguments!\n");
 		return (1);
 	}
-	pid = ft_atoi(argv[1]);
-	if (!pid)
+	server_pid = ft_atoi(argv[1]);
+	if (!server_pid)
 	{
 		ft_printf("The PID is not correct!\n");
 		return (1);
 	}
 	message = argv[2];
-	send_message(pid, message);
+    while (message[i] != '\0')
+    {
+        send_message(server_pid, message[i]);
+		i++;
+    }
 	return (0);
 }
--- server.c
+++ server.c
@@ -8,18 +8,14 @@ void	handle_signal(int sig)
 	static int	bits_received = 0;
 	static char	current_char = 0;
 
-	// SIGUSR1 is bit 0, SIGUSR2 is bit 1
 	if (sig == SIGUSR1)
 		current_char |= (0 << bits_received);
 	else if (sig == SIGUSR2)
 		current_char |= (1 << bits_received);
-
 	bits_received++;
-
-	// Once we receive 8 bits, we can print the character
 	if (bits_received == 8)
 	{
-		write(1, &current_char, 1); // Write the character to stdout
+		write(1, &current_char, 1);
 		bits_received = 0;
 		current_char = 0;
 	}
