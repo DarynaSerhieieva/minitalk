84e41f13a041f0d26585cd6022840d32e7cafd82
DarynaSerhieieva <mpimkas@gmail.com>
2024-10-10 16:31:57 +0300

    added gitignore file, and wrote some code

--- client.c
+++ client.c
@@ -1,6 +1,39 @@
 #include "libft.h"
 #include "minitalk.h"
 
+void send_char(pid_t server_pid, char c)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        if (c & (1 << i))
+            kill(server_pid, SIGUSR2); // Send bit 1 as SIGUSR2
+        else
+            kill(server_pid, SIGUSR1); // Send bit 0 as SIGUSR1
+
+        usleep(100); // Give the server time to process the signal
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        printf("Usage: %s <server_pid> <message>\n", argv[0]);
+        return 1;
+    }
+
+    pid_t server_pid = atoi(argv[1]);
+    char *message = argv[2];
+
+    // Send each character of the message
+    for (int i = 0; message[i] != '\0'; i++)
+    {
+        send_char(server_pid, message[i]);
+    }
+
+    return 0;
+}
+
 int	main(int argc, char **argv)
 {
 	char				message;
--- server.c
+++ server.c
@@ -1,19 +1,39 @@
 #include "minitalk.h"
 #include "libft.h"
 
-void	sig_proc()
-{
+volatile sig_atomic_t g_bit = 0;
 
+void	handle_signal(int sig)
+{
+	static int	bits_received = 0;
+	static char	current_char = 0;
+
+	// SIGUSR1 is bit 0, SIGUSR2 is bit 1
+	if (sig == SIGUSR1)
+		current_char |= (0 << bits_received);
+	else if (sig == SIGUSR2)
+		current_char |= (1 << bits_received);
+
+	bits_received++;
+
+	// Once we receive 8 bits, we can print the character
+	if (bits_received == 8)
+	{
+		write(1, &current_char, 1); // Write the character to stdout
+		bits_received = 0;
+		current_char = 0;
+	}
 }
 
 int	main(void)
 {
-	pid_t	pid;
+	pid_t	server_pid;
 
-	pid = getpid();
-	ft_printf("PID: %u\n", pid);
-	signal(SIGUSR1, sig_proc);
-	signal(SIGUSR2, sig_proc);
+	server_pid = getpid();
+	ft_printf("PID: %u\n", server_pid);
+	signal(SIGUSR1, handle_signal);
+	signal(SIGUSR2, handle_signal);
 	while (1)
 		pause();
+	return (0);
 }
